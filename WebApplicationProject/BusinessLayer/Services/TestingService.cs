b8b6e765edb6173ca54a8c6506f5289324d34ee5
oscarjnz <oscarjimenez1410@gmail.com>
2025-05-21 20:29:07 -0400

    commit

--- WebApplicationProject/BusinessLayer/Services/TestingService.cs
+++ WebApplicationProject/BusinessLayer/Services/TestingService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLayer.DTO;
 
-namespace BusinessLayer
+namespace BusinessLayer.Services
 {
-    public class Class1
+    internal class TestingService
     {
+        public TestingService() { }
     }
 }
---
+++ WebApplicationProject/ModelLayer/DTO/TestingModelView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO
+{
+    internal class TestingModelView
+    {
+     public int ClientId { get; set; }
+    }
+}
